5f8339095d30a8ad8447ff5140a1397f0d1c40fc
Balint Rikker <rikkerb@gmail.com>
2017-05-09 01:17:43 +0200

    Lichtenstein re-joins Europe

--- kalkulilo.js
+++ kalkulilo.js
@@ -120,7 +120,7 @@ function getCountryCategoryFromCountryCode(country) {
     // B: european countries ekc. kat. A
     var b_countries = [ 'AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE',
                         'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR',
-                        'IE', 'IS', 'IT', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PL',
+                        'IE', 'IS', 'IT', 'LI', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PL',
                         'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA' ];
 
     // B: cxio alia
